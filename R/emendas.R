806eff4715f44d150e5056f7fa1fe3fbd2909e31
Renata H <re.hirota@gmail.com>
2019-10-23 18:58:08 -0300

    add emendas do senado

--- R/emendas.R
+++ R/emendas.R
@@ -1,5 +1,6 @@
 library(jsonlite)
 library(dplyr)
+library(tidyr)
 library(stringr)
 
 # Proposições relacionadas à PEC00619
@@ -54,4 +55,65 @@ autores_proponentes %>%
   group_by(id_deputado, nome.x, siglaPartido) %>%
   summarise(n = n_distinct(id_emenda)) %>%
   arrange(desc(n))
+
+
+# Emendas no Senado ----
+
+url <- "http://legis.senado.leg.br/dadosabertos/materia/emendas/137999"
+json <- fromJSON(url)
+
+emendas <- json$EmendaMateria$Materia$Emendas$Emenda
+autoria <- emendas$AutoriaEmenda$Autor
+
+dados <- data.frame()
+for (i in seq(1,594)) {
+  CodigoEmenda <- emendas$CodigoEmenda[[i]]
+  autor <- data.frame(autoria[[i]][[6]], CodigoEmenda)
+  dados <- bind_rows(dados, autor)
+}
+
+emendas_autor <- emendas %>% select(1:6) %>% left_join(dados) %>%
+  janitor::clean_names()
+emendas_autor %>% glimpse()
+
+
+# Total de emendas por autor
+emendas_autor %>%
+  group_by(codigo_parlamentar, nome_parlamentar, sigla_partido_parlamentar) %>%
+  summarise(n = n_distinct(numero_emenda)) %>%
+  arrange(desc(n))
+
+# Total de emendas por partido
+emendas_autor %>%
+  group_by(sigla_partido_parlamentar) %>%
+  summarise(n = n_distinct(numero_emenda)) %>%
+  arrange(desc(n))
+emendas_autor %>% filter(is.na(sigla_partido_parlamentar)) %>% glimpse()
+
+# Alguns senadores estão sem o partido, buscar na api
+url <- "http://legis.senado.leg.br/dadosabertos/senador/lista/atual"
+json <- fromJSON(url)
+senadores <- json$ListaParlamentarEmExercicio$Parlamentares$Parlamentar$IdentificacaoParlamentar
+senadores <- senadores %>%
+  select(CodigoParlamentar, NomeParlamentar, SiglaPartidoParlamentar) %>%
+  janitor::clean_names()
+
+emendas_autor <- emendas_autor %>%
+  left_join(senadores, by = 'codigo_parlamentar')
+
+emendas_autor <- emendas_autor %>%
+  mutate(sigla_partido_parlamentar = ifelse(is.na(sigla_partido_parlamentar.x),
+                                    sigla_partido_parlamentar.y,
+                                    sigla_partido_parlamentar.x))
+
+# Emendas por partido
+emendas_autor %>%
+  group_by(sigla_partido_parlamentar) %>%
+  summarise(n = n_distinct(numero_emenda)) %>%
+  arrange(desc(n))
+
+
+
+
+
 
